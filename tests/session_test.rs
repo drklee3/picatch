0dfd874b3a37d7f63f04046f461950ca641826b1
drklee3 <derrick@dlee.dev>
2020-02-03 20:53:26 -0800

    test: Update session tests with new session::new params

--- tests/session_test.rs
+++ tests/session_test.rs
@@ -1,15 +1,13 @@
 use chrono::Utc;
 use dphoto_lib::model::store::Session;
-use std::net::{IpAddr, Ipv4Addr};
 
 #[test]
 fn it_generates_random_session_id() {
-    let ip = IpAddr::from(Ipv4Addr::new(192, 168, 1, 1));
     let now = Utc::now().naive_utc();
-    let user_agent = "Test user agent".to_owned();
+    let user_agent = Some("Test user agent".to_owned());
 
-    let ses1 = Session::new(1, ip, user_agent.clone(), now.clone());
-    let ses2 = Session::new(2, ip, user_agent.clone(), now.clone());
+    let ses1 = Session::new(1, None, user_agent.clone(), now.clone());
+    let ses2 = Session::new(2, None, user_agent.clone(), now.clone());
     assert!(ses1.id != ses2.id);
     println!("{}\n{}", ses1.id, ses2.id);
 }
