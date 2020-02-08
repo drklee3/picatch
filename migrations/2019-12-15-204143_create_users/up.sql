6208e3484610b46226e295f894cd84cc134dc0e8
drklee3 <derrick@dlee.dev>
2020-02-08 14:01:01 -0800

    fix: Make usernames unique

--- migrations/2019-12-15-204143_create_users/up.sql
+++ migrations/2019-12-15-204143_create_users/up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE users (
     id         SERIAL     PRIMARY KEY,
-    username   TEXT       NOT NULL,
+    username   TEXT       UNIQUE NOT NULL,
     hash       TEXT       NOT NULL,
     created_at TIMESTAMP  NOT NULL
 )
