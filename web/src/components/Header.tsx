18e2f6fc9ee809be235d252490a4ac78f3abb4fe
drklee3 <derrick@dlee.dev>
2020-06-25 22:35:56 -0700

    feat(web): Navigate home via header logo click

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -1,13 +1,23 @@
 import React from "react";
 import Breadcrumbs, { BreadcrumbsProps } from "./Breadcrumbs";
+import { ActiveFileActions } from "../reducers/activeFileActions";
 import Links from "./Links";
 
 type HeaderProps = BreadcrumbsProps;
 
 function Header(props: HeaderProps) {
+    function navigateHome() {
+        props.dispatch({
+            type: ActiveFileActions.SET_ALBUM,
+            album: "/",
+        });
+    }
+
     return (
         <header>
-            <h3>picatch</h3>
+            <div>
+                <h3 onClick={navigateHome}>picatch</h3>
+            </div>
             <Breadcrumbs {...props} />
             <Links />
         </header>
